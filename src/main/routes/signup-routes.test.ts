75e6e51cd12f8b2d17bd1fdaec01adeb11cd0e18
Alexandre Akira <alexandreakiraakao@gmail.com>
2021-06-29 00:43:36 -0300

    test: ensure SignUp Route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Alexandre',
+        email: 'alexandreakira@mail.com',
+        password: '12345',
+        passwordConfirmation: '12345'
+      })
+      .expect(200)
+  })
+})
