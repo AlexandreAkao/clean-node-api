d4f4f3db775070357c806f14d404d510a771a50b
Alexandre Akira <alexandreakiraakao@gmail.com>
2021-06-29 00:26:30 -0300

    feat: ensure requests return defaut content type as json

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -5,7 +5,7 @@ import app from '../config/app'
 describe('Cors Middleware', () => {
   test('Should enable cors', async () => {
     app.get('/test_cors', (req, res) => {
-      res.send(req.body)
+      res.send()
     })
 
     await request(app)
