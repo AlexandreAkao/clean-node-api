f20417ec119c52195400e22cda1145a13e1192b4
Alexandre Akira <alexandreakiraakao@gmail.com>
2021-07-16 16:36:57 -0300

    feat: ensure BcryptAdapter return true when compare success

--- src/infra/criptography/bcrypt-adpter.spec.ts
+++ src/infra/criptography/bcrypt-adpter.spec.ts
@@ -52,4 +52,11 @@ describe('BcryptAdapter', () => {
 
     expect(compareSpy).toHaveBeenLastCalledWith('any_value', 'any_hash')
   })
+
+  test('Should return true when compare success', async () => {
+    const sut = makeSut()
+    const isValid = await sut.compare('any_value', 'any_hash')
+
+    expect(isValid).toBe(true)
+  })
 })
