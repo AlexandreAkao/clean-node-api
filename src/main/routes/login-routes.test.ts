76e0a616c8185f92c794194b3702cc70f7f39e86
Alexandre Akira <alexandreakiraakao@gmail.com>
2021-07-17 01:41:31 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -52,5 +52,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'alexandreakira@mail.com',
+          password: '12345'
+        })
+        .expect(401)
+    })
   })
 })
