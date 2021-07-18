1ca0c3023f8a04d70fad06474d1965b7b889847c
Alexandre Akira <alexandreakiraakao@gmail.com>
2021-07-18 19:49:20 -0300

    fix: fix default MONGO_URL

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongo:27017/clean-node-api',
   port: process.env.PORT ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? '77d083e3870cc5b3fe0179888143c52b'
 }
